2a31f5686ff4ffdbba57553068389cf02ef70203
Adnan Humačkić <humackic.adnan@gmail.com>
2024-08-28 15:56:34 +0200

    Prikazani proizvodi na product_list_screen-u, urađen search po nazivu

---
+++ naTanjir/UI/natanjir_desktop/lib/models/proizvod.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'proizvod.g.dart';
+
+@JsonSerializable()
+class Proizvod {
+  int? proizvodId;
+  String? naziv;
+  String? slika;
+  double? cijena;
+  int? vrstaProizvodaId;
+  Proizvod({this.proizvodId, this.naziv});
+
+  factory Proizvod.fromJson(Map<String, dynamic> json) =>
+      _$ProizvodFromJson(json);
+
+  /// Connect the generated [_$PersonToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$ProizvodToJson(this);
+}
